bdb7d90495015c8009f5a0f79b50b39e9d6c06ab
aziznawawi <aziznawawi684@gmail.com>
2025-10-02 09:49:54 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 02 Okt 2025 pada 04.45
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
